3cff7fdefc3bb00ad0ef890a6a49009f9848b120
Zachary Fuller <zachary.fuller.dev@gmail.com>
2022-06-23 22:17:26 -0400

    Add third party repo for installing neovim

--- nvim-setup.sh
+++ nvim-setup.sh
@@ -13,6 +13,7 @@ fi
 # TODO: Make sure apt is up to date
 
 # Install neovim and plugin manager
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt install neovim -y
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
